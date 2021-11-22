76bd9990ae4fdabe29a3313f00ba14e5c6b1345f
fpcemdrd01 <94504116+fpcemdrd01@users.noreply.github.com>
2021-11-22 14:26:26 +0800

    Update index1.js

--- demo-02/index1.js
+++ demo-02/index1.js
@@ -119,7 +119,7 @@ function init() {
 
   // 簡單的地板
   const planeGeometry = new THREE.PlaneGeometry(60, 60)
-  const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff })
+  const planeMaterial = new THREE.MeshLambertMaterial({ color: 0x00ffff })
   let plane = new THREE.Mesh(planeGeometry, planeMaterial)
   plane.rotation.x = -0.5 * Math.PI
   plane.position.set(0, -7, 0)
